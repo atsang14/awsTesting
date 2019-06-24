fab53afa13c6ad84cc150225c95e8fa8997d88a6
Austin Tsang <aytsang14@gmail.com>
2019-06-24 16:19:04 -0700

    created a client folder that has react in it

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-// const express = require('express')
-// const app = express()
-// app.get('/', (req, res) => {
-//   res.send('HEY!')
-// })
-// app.listen(3000, () => console.log('Server running on port 3000'))
-
 const express = require('express')
 const app = express()
 app.use(express.static('public'))
 app.listen(3000, () => console.log('Server running on port 3000'))
+
+// const path = require('path');
+// const app = express();
+
+// // express is not good for production static files, use cdn, or dedicated file server like ngnix, appache
+// app.use(express.static(path.join(__dirname, '..', 'build')));
+
+// app.get('*', function (req, res) {
+//   res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+// });
